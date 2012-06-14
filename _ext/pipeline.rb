fd6d70a3de5c978ad895067d0678e0111d21ce93
Paul Dijou <paul.dijou.dev@gmail.com>
2012-06-14 11:30:13 +0200

    Add "News" section with pagination, RSS, and tags.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,11 @@
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new( '/news' ) 
-  # extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Posts.new( '/news' )
+  extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page=>5 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/news/index', '/news/tags', :per_page=>5)
+  extension Awestruct::Extensions::TagCloud.new( :posts, '/news/tags/index.html', :layout=>'base' )
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/news.atom', :num_entries=>20 )
+  extension Awestruct::Extensions::Indexifier.new
+  helper Awestruct::Extensions::Partial
 end
 
