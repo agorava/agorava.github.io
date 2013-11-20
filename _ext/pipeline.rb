95b8230f3eef53999de515f6f4be434416cf903d
Antoine Sabot-Durand <antoine@sabot-durand.net>
2013-11-20 23:21:25 +0100

    Finishing release and correcting layout for bootstrap 3

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -6,7 +6,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Tagger.new( :posts, '/news/index', '/news/tags', :per_page=>5)
   extension Awestruct::Extensions::TagCloud.new( :posts, '/news/tags/index.html', :layout=>'base' )
   extension Awestruct::Extensions::Atomizer.new( :posts, '/news.atom', :num_entries=>20 )
-  extension Awestruct::Extensions::Indexifier.new(['apidocs'])
+  extension Awestruct::Extensions::Indexifier.new(['doc/apidocs070'])
   extension Awestruct::Extensions::Disqus.new()
   extension Awestruct::Extensions::Sitemap.new
   helper Awestruct::Extensions::Partial
